d2e495b1666a6af1cb86c698ca0deb18f6319dec
Bruno Rafante <rafante2@gmail.com>
2021-02-07 18:20:34 -0400

    adds swagger to the project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('PayGo Integrator')
+    .setDescription('API designated to communicate with PayGo backend')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-client', app, document);
+
+  let httpPort = process.env.HTTP_PORT ?? 3000
+  await app.listen(httpPort);
+
+  Logger.log(`Api running on port ${httpPort}`)
 }
 bootstrap();
