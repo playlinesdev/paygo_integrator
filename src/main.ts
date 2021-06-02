36ff1ffb393afaa946a19d62cd32731d4becfd49
rafante <rafante2@gmail.com>
2021-06-02 12:24:06 -0400

    changes the endpoint of the api client

--- src/main.ts
+++ src/main.ts
@@ -31,7 +31,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api-client', app, document);
+  SwaggerModule.setup('api', app, document);
 
   await app.init();
 
